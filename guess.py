631f356c767ed73cc0f3f4cf849da855f569232d
Thomas Stassin <thomas.stassin@gsmproductions.com>
2021-05-12 13:57:47 +0200

    power up dev

--- guess.py
+++
@@ -1,7 +0,0 @@
-from random import randint
-
-nombre = input("Entrez un nombre entre 1 et 10: ")
-if nombre == randint(1,10):
-    print("Yay!")
-else:
-    print("Raté !")
--- test.py
+++ test.py
@@ -2,5 +2,7 @@ print("Hello :D")
 print("World!")
 
 answer = input("Comment vas-tu?")
+print("Yo :D")
+
 
 
